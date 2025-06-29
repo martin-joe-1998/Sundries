6520bd054ddcebee2d12bafdf61efa244985fb1f
martin-joe-1998 <bei1998@outlook.com>
2025-06-29 18:05:07 +0900

    update

---
+++ OpticalGame/OpticalGame/Rectangle.cpp
@@ -0,0 +1,19 @@
+﻿#include "Rectangle.h"
+#include "Math.h"
+#include <fstream>
+#include <vector>
+
+struct Vertex
+{
+	Vector3 position; // Position
+	Vector2 uv;    // UV
+	Vector3 color; // Color
+};
+
+Rectangle::Rectangle()
+{
+}
+
+Rectangle::~Rectangle()
+{
+}
---
+++ OpticalGame/OpticalGame/Rectangle.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Renderer.h"
+
+class Rectangle
+{
+public:
+    Rectangle();
+	~Rectangle();
+    void Draw(Renderer& renderer);
+private:
+};
+
