f2a2925f7428e51178930731aed061093fef142f
martin-joe-1998 <bei1998@outlook.com>
2025-07-14 22:51:15 +0900

    delete unchanged file

--- Sonoxxx/C++Project/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include <Windows.h>
-#include "Application/Application.h"
-#include "Utility/Math.h"
-#include "Utility/Logger.h"
-#include <iostream>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
-  return sl::Application::GetInstance(hInstance, nCmdShow).Run();
-}
